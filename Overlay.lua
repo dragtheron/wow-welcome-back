3581318522264585f7ddbe2de2eda2c5e0576177
Tobias Stettner <dev@tsno.de>
2023-11-16 01:08:01 +0100

    feat: prepare overlay

--- Overlay.lua
+++ Overlay.lua
@@ -4,22 +4,25 @@ local frame = CreateFrame("Frame", "WelcomeBack_Overlay", UIParent)
 frame:SetPoint("TOPLEFT", 40, -40)
 frame:SetSize(300, 40)
 
-frame.ActivityLine = frame:CreateFontString(nil, "BACKGROUND", "GameFontNormal")
+frame.ActivityLine = frame:CreateFontString(nil, "BACKGROUND", "GameFontNormalSmall")
 frame.ActivityLine:SetPoint("TOPLEFT", 0, 0)
-frame.ActivityLine:SetHeight(16)
-frame.ActivityLine:SetText("Loading...")
+frame.ActivityLine:SetHeight(12)
+frame.ActivityLine:SetText("Loading Activity Info...")
+frame.ActivityLine:SetJustifyH("LEFT")
 
-frame.ActivityLine:SetWidth()
-frame.ActivityProgress = frame:CreateFontString(nil, "BACKGROUND", "GameFontNormal")
+frame.ActivityLine:SetWidth(frame:GetWidth())
+frame.ActivityProgress = frame:CreateFontString(nil, "BACKGROUND", "GameFontHighlight")
 frame.ActivityProgress:SetPoint("TOPLEFT", frame.ActivityLine, "BOTTOMLEFT", 0, -8)
+frame.ActivityProgress:SetWidth(frame:GetWidth())
 frame.ActivityProgress:SetHeight(16)
-frame.ActivityLine:SetText("Loading...")
+frame.ActivityProgress:SetText("...")
+frame.ActivityProgress:SetJustifyH("LEFT")
 
 local function onUpdate()
     local lastActivity = addon.HaveWeMet.lastActivity
-    local activityName = addon.HaveWeMet.GetActivityTitle(lastActivity)
 
     if lastActivity then
+        local activityName = addon.HaveWeMet.GetActivityTitle(lastActivity)
         frame.ActivityLine:SetText(format("Current Activity: |cffffffff%s|r", activityName))
         frame.ActivityLine:SetShown(true)
 
@@ -40,13 +43,22 @@ local function onUpdate()
                 local detailsString = addon.HaveWeMet.GetDetailsString(playerLastActivity)
                 frame.ActivityProgress:SetText(detailsString)
             end
+
+            frame.ActivityProgress:SetShown(true)
+        else
+            frame.ActivityProgress:SetShown(false)
         end
 
-        frame:SetShown(true)
+        frame.ActivityLine:SetShown(true)
     else
-        frame:SetShown(false)
+        frame.ActivityLine:SetShown(false)
+        frame.ActivityProgress:SetShown(false)
     end
 end
+
+addon.Overlay = {
+    frame = frame
+}
 
 EventRegistry:RegisterCallback(addonName .. ".HaveWeMet.Update", onUpdate)
 EventRegistry:RegisterCallback(addonName .. ".HaveWeMet.ActivityUpdate", onUpdate)
